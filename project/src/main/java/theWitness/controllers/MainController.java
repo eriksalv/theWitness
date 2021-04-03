53c2a07125062b2716acfaf803cb11e637bf3d06
Asomiraia <erik.salvesen@hotmail.no>
2021-04-03 21:51:27 +0200

    continue knapp

--- project/src/main/java/theWitness/controllers/MainController.java
+++ project/src/main/java/theWitness/controllers/MainController.java
@@ -8,18 +8,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import theWitness.GameCollection;
 
 public class MainController {
+	@FXML private Button continueButton;
 	
 	@FXML private SaveGameController saveGameController;
 	
 	private GameCollection games;
 	
+	@FXML
+	private void initialize() {
+		
+	}
+	
 	public void initData(GameCollection games) {
 		this.games=games;
+		if (games!=null) {
+			continueButton.setVisible(true); //det skal ikke gå an å trykke på continue når en fil med en gamecollection ikke er satt edna.
+		}
 	}
 	
 	@FXML
@@ -42,7 +52,26 @@ public class MainController {
 	}
 	
 	@FXML
-	private void handleNewGame(MouseEvent e) throws IOException {
+	private void handleContinue(ActionEvent e) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/Game.fxml"));
+        Parent tableViewParent = loader.load();
+        
+        Scene tableViewScene = new Scene(tableViewParent);
+        
+        //access the controller and call a method
+        GameController controller = loader.getController();
+        controller.initData(games);
+        
+        //This line gets the Stage information
+        Stage window = (Stage)((Node)e.getSource()).getScene().getWindow();
+        
+        window.setScene(tableViewScene);
+        window.show();
+	}
+	
+	@FXML
+	private void handleNewGame(ActionEvent e) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/fxml/Game.fxml"));
         Parent tableViewParent = loader.load();
