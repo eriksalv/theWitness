8aa03f420827b6e6f60e3fb9bc79082699cddf7b
Erik Salvesen <eriksalv@stud.ntnu.no>
2021-04-23 16:58:47 +0200

    more levels

--- project/src/main/java/theWitness/LevelEnumerator.java
+++ project/src/main/java/theWitness/LevelEnumerator.java
@@ -12,7 +12,7 @@ public enum LevelEnumerator implements LevelSettings, ISaveHandler {
 	
 	EMPTY, INTEGRATIONTEST, LEVEL_1, LEVEL_2, LEVEL_3, LEVEL_4, LEVEL_5, LEVEL_6,
 	LEVEL_7, LEVEL_8, LEVEL_9, LEVEL_10, LEVEL_11, LEVEL_12, LEVEL_13, LEVEL_14,
-	LEVEL_15, LEVEL_16, LEVEL_17;
+	LEVEL_15, LEVEL_16, LEVEL_17, LEVEL_18, LEVEL_19, LEVEL_20;
 	
 	//public final static String SAVE_FOLDER = "src/main/resources/config/";
 	public final static String SAVE_FOLDER = "config/";
